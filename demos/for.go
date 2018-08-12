b3730e72e2c9fe5a29e41a44304aa6b6344b8455
yujinghui <jinghuiace@gmail.com>
2018-08-12 11:58:57 +0800

    go;

--- demos/for.go
+++ demos/for.go
@@ -4,7 +4,7 @@ package main
 import "fmt"
 
 func main() {
-	for i:=1; i < 100; i++ {
+	for i:=2; i < 100; i++ {
 		fmt.Println(i)
 	}
 }
---
+++ demos/relection.go
@@ -0,0 +1,12 @@
+
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int =9;
+	ii :=9;
+	fmt.Println(i)
+	fmt.Println(ii)
+}
